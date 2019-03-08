841c29fa28c159de84b88f86bd95762996d836c6
Mauricio Oliveira <oliveiramauricio72@gmail.com>
2019-03-08 17:20:23 -0300

    (GithubCompare): busca repositorios api

---
+++ src/pages/Main/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import api from '../../services/api';
+import logo from '../../assets/logo.png';
+
+import { Container, Form } from './styles';
+import CompareList from '../../components/CompareList/index';
+
+export default class Main extends Component {
+  state = {
+    repositoryInput: '',
+    repositories: [],
+  };
+
+  handleAddRepository = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await api.get(`/repos/${this.state.repositoryInput}`);
+
+      this.setState({
+        repositoryInput: '',
+        repositories: [...this.state.repositories, response.data],
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  render() {
+    return (
+      <Container>
+        <img src={logo} alt="Github Compare" />
+
+        <Form onSubmit={this.handleAddRepository}>
+          <input
+            type="text"
+            placeholder="usuário/repositorio"
+            value={this.state.repositoryInput}
+            onChange={e => this.setState({ repositoryInput: e.target.value })}
+          />
+          <button type="submit">OK</button>
+        </Form>
+
+        <CompareList repositories={this.state.repositories} />
+      </Container>
+    );
+  }
+}
